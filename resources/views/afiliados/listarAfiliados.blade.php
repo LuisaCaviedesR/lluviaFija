fa8feb9ee3961f8cfa339d9cd33dfeea6fb91f81
erikacaicedo <erikacaicedo88@gmail.com>
2017-03-05 20:35:32 -0500

    actualice listar afliados

--- resources/views/afiliados/listarAfiliados.blade.php
+++ resources/views/afiliados/listarAfiliados.blade.php
@@ -3,39 +3,46 @@
 
   <div class="container">
 	<div class="page-header">
-       <h1>Tablas y estilos en <small>Boostrap 3</small></h1>
+       <h1>Listar Afiliados</h1>
     </div>
 	
-<table class="table">
-    <thead>
-        <tr>
-            <th>#</th>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Email</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>1</td>
-            <td>Rocky</td>
-            <td>Balboa</td>
-            <td>rockybalboa@mail.com</td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>Peter</td>
-            <td>Parker</td>
-            <td>peterparker@mail.com</td>
-        </tr>
-        <tr>
-            <td>3</td>
-            <td>John</td>
-            <td>Rambo</td>
-            <td>johnrambo@mail.com</td>
-        </tr>
-    </tbody>
-</table>
+<table class="table table-hover" >
+             <tr>
+                <td colspan="6">A continuación encotrara los afiliados de nuestro parque</td>
+            </tr>
+            <tr>
+                <th>id</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Email</th>
+                <th>Celular</th>
+                <th>Acciones</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>John</td>
+                <td>Boo</td>
+                <td>johnny81@gmail.com</td>
+                <td>xxx-xxxxxxx</td>
+                <td><input type="radio"></td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Mary</td>
+                <td>Brown</td>
+                <td>missmary@gmail.com</td>
+                <td>xxx-xxxxxxx</td>
+                <td><input type="radio"></td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>James</td>
+                <td>Mooray</td>
+                <td>jijames@gmail.com</td>
+                <td>xxx-xxxxxxx</td>
+                <td><input type="radio"></td>
+            </tr>
+        </table>
 
   </div>
 @stop
