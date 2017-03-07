6d1bb5c448018853183452df0ca2df9da9dabad7
estefania <estefania@starenweb.com>
2017-03-06 21:02:57 -0500

    Vista maquetada listar cabañas

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -7,7 +7,12 @@
                 "url": "https://cdn.datatables.net/plug-ins/1.10.13/i18n/Spanish.json"
                 }
         });
-
-        
+          $('#cabana').DataTable({
+                    responsive: true,
+                    "scrollX": false,
+                    "language": {
+                    "url": "https://cdn.datatables.net/plug-ins/1.10.13/i18n/Spanish.json"
+                    }
+            });
 
     });
--- resources/views/cabanas/listarCabanas.blade.php
+++ resources/views/cabanas/listarCabanas.blade.php
@@ -1,4 +1,45 @@
 @extends ('layouts.app')
 @section('content')
+<p></p>
+        <div class="row">
+   	        <div class="col-lg-12 col-md-12 col-sm-12">
+   		       <div class="panel panel-default">
+   			      <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-sm-9">Cabañas</div>
+                        <div class="col-sm-3">
+                            <button type="button" class="btn btn-primary">
+                                Registrar Cabaña <i class="fa fa-plus-circle"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+           	    <div class="panel-body">
+                <div class="table-responsive col-lgl-12">
+           		  <table class="table table-striped table-bordered table-hover" id="cabana">
+	 		        <thead>
+				        <tr>
+				            <th width="16%"># Cabaña</th>
+				            <th width="16%">Capacidad</th>
+				            <th width="18%">Descripción</th>
+                            <th width="16%">Disponible</th>
+				            <th width="16%">Valor</th>
+                            <th width="16%">Acciones</th>
+				        </tr>
+	 		        </thead>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td><input type="radio"></td>
+                    </tr>
+				    </table>
+                    </div>
+			  </div>
+		  </div>
+	   </div>
+    </div>
 
 @stop
