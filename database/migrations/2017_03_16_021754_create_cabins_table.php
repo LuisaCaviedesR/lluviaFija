2dc712cfb9c6de18188537544db59d5bed9fc5fa
estefaniamarinp <tefamarin@gmail.com>
2017-03-15 21:23:24 -0500

    Se configura la base de datos local, se crea la migracion de Cabins y los campos de Cabins

---
+++ database/migrations/2017_03_16_021754_create_cabins_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCabinsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cabins', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('cabin_id')->unique();
+            $table->string('description');
+            $table->integer('capacity');
+            $table->float('price',11,2)->default(0.0);
+            $table->boolean('available');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cabins');
+    }
+}
