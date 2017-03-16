de166cd2ac3d855e8f2c54dd717debe43ca37b8c
Luisa Fernanda Caviedes Restrepo <lcaviedesrestrepo@gmail.com>
2017-03-15 21:16:38 -0500

    Alquiler migracion y modelo

---
+++ database/migrations/2017_03_16_015637_create_rentals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRentalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rentals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('days');
+            $table->integer('nights');
+            $table->float('price', 11, 2)->default(0.0);
+            $table->integer('quantity');
+            $table->date('datein');
+            $table->date('dateout');
+            $table->timestamps();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
