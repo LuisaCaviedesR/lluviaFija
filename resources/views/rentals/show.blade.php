6baeb900f2f68d4747b081215c01d2e52f161e01
Luisa Fernanda Caviedes Restrepo <lcaviedesrestrepo@gmail.com>
2017-03-19 18:43:42 -0500

    rentals

---
+++ resources/views/rentals/show.blade.php
@@ -0,0 +1,53 @@
+@extends ('layouts.app')
+@section('content')
+<p></p>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">Detalle Alquiler</div>
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-6 col-sm-6">
+                                <table class="table table-striped table-bordered table-hover">
+                                    <tr>
+                                        <td>Afiliado</td>
+                                        <td></td>
+                                        <td>Identificación</td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Cabaña</td>
+                                        <td></td>
+                                        <td>Capacidad max</td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Fecha Ingreso</td>
+                                        <td>{{$data->datein}}</td>
+                                        <td>Fecha Salida</td>
+                                        <td>{{$data->dateout}}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Personas</td>
+                                        <td>{{$data->quantity}}</td>
+                                        <td>Valor</td>
+                                        <td>{{$data->price}}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Días</td>
+                                        <td>{{$data->days}}</td>
+                                        <td>Noches</td>
+                                        <td>{{$data->nights}}</td>
+                                    </tr>
+				                </table>  
+                                <button type="button" class="btn btn-primary" onclick="location.href='{!! url('rentals') !!}'">
+                                Regresar
+                            </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+      </div>        
+
+@stop
