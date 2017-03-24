2b0eb9e82feb212e6a8ab5fc4e133065d9da3d4d
Luisa Fernanda Caviedes Restrepo <lcaviedesrestrepo@gmail.com>
2017-03-24 00:50:07 -0500

    roles

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,8 +23,7 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
             $table->foreign('rol_id')->references('id')->on('roles')
-                   ->onUpdate('cascade')
-                   ->onDelete('cascade');
+                   ->onUpdate('cascade');
         });
     }
 
